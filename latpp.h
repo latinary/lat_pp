050d649c088c9aaab682f25686660ac99f2b35df
damix69420 <damix4921@gmail.com>
2022-08-20 23:19:01 +0200

    latpp

---
+++ latpp.h
@@ -0,0 +1,20 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+using namespace std;
+
+#define filum string;
+
+namespace vexillum
+{
+    using namespace std;
+    void dicunt(string x)
+    {
+        cout << x << endl;
+    }
+    auto input(string s, string x)
+    {
+        return x;
+    }
+}
